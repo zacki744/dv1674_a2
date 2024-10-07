d97839b27816c418f361df2b28b83483214b4829
Suejb Memeti <sm13294@gmail.com>
2024-10-07 12:37:50 +0200

    fixes some issues rated to missing libraries

--- pearson/dataset.cpp
+++ pearson/dataset.cpp
@@ -7,53 +7,61 @@ Author: David Holmqvist <daae19@student.bth.se>
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <iomanip>
+#include <limits>
 
-namespace Dataset {
-std::vector<Vector> read(std::string filename)
+namespace Dataset
 {
-    unsigned dimension {};
-    std::vector<Vector> result {};
-    std::ifstream f {};
+    std::vector<Vector> read(std::string filename)
+    {
+        unsigned dimension{};
+        std::vector<Vector> result{};
+        std::ifstream f{};
 
-    f.open(filename);
+        f.open(filename);
 
-    if (!f) {
-        std::cerr << "Failed to read dataset(s) from file " << filename << std::endl;
+        if (!f)
+        {
+            std::cerr << "Failed to read dataset(s) from file " << filename << std::endl;
+            return result;
+        }
+
+        f >> dimension;
+        std::string line{};
+
+        std::getline(f, line); // ignore first newline
+
+        while (std::getline(f, line))
+        {
+            std::stringstream ss{line};
+            Vector new_vec{dimension};
+            std::copy_n(std::istream_iterator<double>{ss},
+                        dimension,
+                        new_vec.get_data());
+            result.push_back(new_vec);
+        }
+
         return result;
     }
 
-    f >> dimension;
-    std::string line {};
+    void write(std::vector<double> data, std::string filename)
+    {
+        std::ofstream f{};
 
-    std::getline(f, line); // ignore first newline
+        f.open(filename);
 
-    while (std::getline(f, line)) {
-        std::stringstream ss { line };
-        Vector new_vec { dimension };
-        std::copy_n(std::istream_iterator<double> { ss },
-            dimension,
-            new_vec.get_data());
-        result.push_back(new_vec);
+        if (!f)
+        {
+            std::cerr << "Failed to write data to file " << filename << std::endl;
+            return;
+        }
+
+        for (auto i{0}; i < data.size(); i++)
+        {
+            f << std::setprecision(std::numeric_limits<double>::digits10 + 1) << data[i] << std::endl;
+        }
     }
-
-    return result;
-}
-
-void write(std::vector<double> data, std::string filename)
-{
-    std::ofstream f {};
-
-    f.open(filename);
-
-    if (!f) {
-        std::cerr << "Failed to write data to file " << filename << std::endl;
-        return;
-    }
-
-    for (auto i { 0 }; i < data.size(); i++) {
-        f << std::setprecision(std::numeric_limits<double>::digits10 + 1) << data[i] << std::endl;
-    }
-}
 
 };
--- pearson/dataset.hpp
+++ pearson/dataset.hpp
@@ -4,13 +4,15 @@ Author: David Holmqvist <daae19@student.bth.se>
 
 #include "vector.hpp"
 #include <string>
+#include <vector>
 
 #if !defined(DATASET_HPP)
 #define DATASET_HPP
 
-namespace Dataset {
-std::vector<Vector> read(std::string filename);
-void write(std::vector<double> data, std::string filename);
+namespace Dataset
+{
+    std::vector<Vector> read(std::string filename);
+    void write(std::vector<double> data, std::string filename);
 };
 
 #endif
