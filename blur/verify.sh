405dd5b173da0f2d4f391621d23f79949fda7487
Suejb Memeti <sm13294@gmail.com>
2024-10-19 18:54:18 +0200

    small fixes

--- blur/verify.sh
+++ blur/verify.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
+echo "NOTE: this script relies on the binaries blur and blur_par to exist"
+
 status=0
 red=$(tput setaf 1)
-green=$(tput setaf 2)
 reset=$(tput sgr0)
-
-counter=0
 
 for thread in 1 2 4 8 16 32
 do
     for image in im1 im2 im3 im4
     do
-        ./blur_par 15 "data/${image}.ppm" "./data_o/${image}_par.ppm" $thread
+        ./blur_par 15 "data/$image.ppm" "./data_o/blur_${image}_par.ppm" $thread
 
-        if ! cmp -s "./data_o/${image}_seq.ppm" "./data_o/${image}_par.ppm"
+        if ! cmp -s "./data_o/blur_${image}.ppm" "./data_o/blur_${image}_par.ppm"
         then
-            echo "${red}Error: Mismatch in output data for image ${image}.ppm with ${thread} thread(s)${reset}"
-            counter=$((counter+1))
+            echo "${red}Error: Incongruent output data detected when blurring image $image.ppm with $thread thread(s)${reset}"
             status=1
         fi
 
-        rm "./data_o/${image}_par.ppm"
+        rm "./data_o/blur_${image}_par.ppm"
     done
 done
-
-if [ $counter -eq 0 ]
-then
-    echo "${green}Success: All tests passed successfully${reset}"
-fi
 
 exit $status
--- pearson/verify.sh
+++ pearson/verify.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 status=0
-red=$(tput setaf 1)
-yellow=$(tput setaf 3)
-reset=$(tput sgr0)
+if tput setaf 1 &> /dev/null; then
+    red=$(tput setaf 1)
+    yellow=$(tput setaf 3)
+    green=$(tput setaf 2)
+    reset=$(tput sgr0)
+else
+    # If tput fails, fall back to no color
+    red=""
+    yellow=""
+    green=""
+    reset=""
+fi
+
+codegrade=1
 
 counter=0
 
@@ -20,63 +31,79 @@ compare_floats() {
         a_frac = substr(a_parts[2], 1, 15)
         b_frac = substr(b_parts[2], 1, 15)
 
-        # Compare the first 13 decimals (considered major part)
+        # Compare the first 11 decimals (considered major part)
         a_major = substr(a_frac, 1, 11)
         b_major = substr(b_frac, 1, 11)
 
-        # Compare the last two decimals separately (considered minor part)
-        a_minor = substr(a_frac, 12, 2)
-        b_minor = substr(b_frac, 12, 2)
+        # Compare the last four decimals separately (considered minor part)
+        a_minor = substr(a_frac, 12, 4)
+        b_minor = substr(b_frac, 12, 4)
 
         if (a_major != b_major) {
             exit 1;  # Major difference
         } else if (a_minor != b_minor) {
-            exit 2;  # Minor difference (last two digits)
+            exit 2;  # Minor difference (last four digits)
         }
 
         exit 0;  # No difference
     }'
 }
 
+# Run pearson to generate sequential output
+./pearson "data/128.data" "./data_o/128_seq.data"
+./pearson "data/256.data" "./data_o/256_seq.data"
+./pearson "data/512.data" "./data_o/512_seq.data"
+./pearson "data/1024.data" "./data_o/1024_seq.data"
+
 for thread in 2 4 8 16 32
 do
     for size in 128 256 512 1024
     do
+        # Run the parallel version
         ./pearson_par "data/$size.data" "./data_o/${size}_par.data" $thread
 
-        # Compare the files line by line
-        difference_found=0
-        warning_issued=0
-        line_num=0
-        while IFS= read -r seq_line && IFS= read -r par_line <&3
-        do
-            line_num=$((line_num + 1))
-            compare_floats "$seq_line" "$par_line"
-            result=$?
-            
-            if [ "$result" -eq 1 ]
-            then
-                # Report major differences as errors and print details
-                echo "${red}Error: Mismatch in output data for size $size with $thread thread(s)${reset}"
-                echo "Expected: $seq_line, Got: $par_line (at line $line_num)"
+        # Use cmp to quickly check if there are any differences at all
+        if cmp -s "./data_o/${size}_seq.data" "./data_o/${size}_par.data"; then
+            echo "Files match for size $size with $thread thread(s)"
+        else
+            # If cmp finds a difference, proceed to compare floats line by line
+            echo "Error: Initial check found differences for size $size with $thread thread(s). Checking details..."
+            if [ $codegrade -eq 1 ]; then
+                echo "${red}Please run the script locally to find more details about such differences and understand if they are acceptable for this submission.${reset}"
                 status=1
-                difference_found=1
                 counter=$((counter+1))
                 break
-                
-
-            elif [ "$result" -eq 2 ]
-            then
-                # Issue warnings for minor differences (last two decimals)
-                if [ "$warning_issued" -eq 0 ]; then
-                    echo "${yellow}Warning: Minor differences (last four digits) detected for size $size and $thread thread(s)${reset}"
-                    warning_issued=1
-                fi
             fi
-        done < "./data_o/${size}_seq.data" 3< "./data_o/${size}_par.data"
 
-        if [ "$difference_found" -eq 0 ]; then
-            echo "Files match for size $size with $thread thread(s)"
+            difference_found=0
+            warning_issued=0
+            line_num=0
+
+            while IFS= read -r seq_line && IFS= read -r par_line <&3
+            do
+                line_num=$((line_num + 1))
+                compare_floats "$seq_line" "$par_line"
+                result=$?
+
+                if [ "$result" -eq 1 ]
+                then
+                    # Report major differences as errors and print details
+                    echo "${red}Error: Mismatch in output data for size $size with $thread thread(s)${reset}"
+                    echo "Expected: $seq_line, Got: $par_line (at line $line_num)"
+                    status=1
+                    difference_found=1
+                    counter=$((counter+1))
+                    break
+
+                elif [ "$result" -eq 2 ]
+                then
+                    # Issue warnings for minor differences (last four decimals)
+                    if [ "$warning_issued" -eq 0 ]; then
+                        echo "${yellow}Warning: Minor differences (last four digits) detected for size $size with $thread thread(s)${reset}"
+                        warning_issued=1
+                    fi
+                fi
+            done < "./data_o/${size}_seq.data" 3< "./data_o/${size}_par.data"
         fi
 
         rm "./data_o/${size}_par.data"
