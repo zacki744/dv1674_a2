0e071505ba0f33f10e24559661a4eb49da4318d5
Suejb Memeti <sm13294@gmail.com>
2024-10-21 13:48:11 +0200

    Update verify.sh

--- pearson/verify.sh
+++ pearson/verify.sh
@@ -14,40 +14,9 @@ else
     reset=""
 fi
 
-codegrade=1
-
 counter=0
-
-# Function to compare floats with precision up to 15 decimal places
-compare_floats() {
-    awk -v a="$1" -v b="$2" 'BEGIN {
-        split(a, a_parts, ".")
-        split(b, b_parts, ".")
-        
-        # Check integer part
-        if (a_parts[1] != b_parts[1]) exit 1;
-        
-        # Compare fractional part up to the first 15 decimals
-        a_frac = substr(a_parts[2], 1, 15)
-        b_frac = substr(b_parts[2], 1, 15)
-
-        # Compare the first 11 decimals (considered major part)
-        a_major = substr(a_frac, 1, 11)
-        b_major = substr(b_frac, 1, 11)
-
-        # Compare the last four decimals separately (considered minor part)
-        a_minor = substr(a_frac, 12, 4)
-        b_minor = substr(b_frac, 12, 4)
-
-        if (a_major != b_major) {
-            exit 1;  # Major difference
-        } else if (a_minor != b_minor) {
-            exit 2;  # Minor difference (last four digits)
-        }
-
-        exit 0;  # No difference
-    }'
-}
+errors_found=0
+warnings_found=0
 
 # Run pearson to generate sequential output
 ./pearson "data/128.data" "./data_o/128_seq.data"
@@ -62,57 +31,37 @@ do
         # Run the parallel version
         ./pearson_par "data/$size.data" "./data_o/${size}_par.data" $thread
 
-        # Use cmp to quickly check if there are any differences at all
-        if cmp -s "./data_o/${size}_seq.data" "./data_o/${size}_par.data"; then
-            echo "Files match for size $size with $thread thread(s)"
+        # Run the verify script and capture the return code
+        ./verify "./data_o/${size}_seq.data" "./data_o/${size}_par.data"
+        ret=$?
+
+        # Check the return code and print corresponding message
+        if [ $ret -eq 2 ]; then
+            echo "${red}ERROR: Significant mismatch found in size ${size} with ${thread} thread(s).${reset}"
+            errors_found=1
+        elif [ $ret -eq 1 ]; then
+            echo "${yellow}WARNING: Minor differences found in size ${size} with ${thread} thread(s).${reset}"
+            warnings_found=1
+        elif [ $ret -eq 0 ]; then
+            echo "${green}Success: Files match for size ${size} with ${thread} thread(s).${reset}"
         else
-            # If cmp finds a difference, proceed to compare floats line by line
-            echo "Error: Initial check found differences for size $size with $thread thread(s). Checking details..."
-            if [ $codegrade -eq 1 ]; then
-                echo "${red}Please run the script locally to find more details about such differences and understand if they are acceptable for this submission.${reset}"
-                status=1
-                counter=$((counter+1))
-                break
-            fi
-
-            difference_found=0
-            warning_issued=0
-            line_num=0
-
-            while IFS= read -r seq_line && IFS= read -r par_line <&3
-            do
-                line_num=$((line_num + 1))
-                compare_floats "$seq_line" "$par_line"
-                result=$?
-
-                if [ "$result" -eq 1 ]
-                then
-                    # Report major differences as errors and print details
-                    echo "${red}Error: Mismatch in output data for size $size with $thread thread(s)${reset}"
-                    echo "Expected: $seq_line, Got: $par_line (at line $line_num)"
-                    status=1
-                    difference_found=1
-                    counter=$((counter+1))
-                    break
-
-                elif [ "$result" -eq 2 ]
-                then
-                    # Issue warnings for minor differences (last four decimals)
-                    if [ "$warning_issued" -eq 0 ]; then
-                        echo "${yellow}Warning: Minor differences (last four digits) detected for size $size with $thread thread(s)${reset}"
-                        warning_issued=1
-                    fi
-                fi
-            done < "./data_o/${size}_seq.data" 3< "./data_o/${size}_par.data"
+            echo "${red}ERROR: An unexpected error occurred while processing size ${size} with ${thread} thread(s).${reset}"
+            errors_found=1
         fi
 
+        # Clean up
         rm "./data_o/${size}_par.data"
     done
 done
 
-if [ $counter -eq 0 ]
-then
-    echo "${green}Success: All tests passed successfully${reset}"
+# Final output based on results
+if [ $errors_found -eq 1 ]; then
+    echo "${red}Errors found during the tests.${reset}"
+    status=1
+elif [ $warnings_found -eq 1 ]; then
+    echo "${yellow}Warnings were found, but no major errors.${reset}"
+else
+    echo "${green}Success: All tests passed successfully.${reset}"
 fi
 
 exit $status
