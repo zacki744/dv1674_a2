32e68b39a260da419c7f95914574231ab0664961
zacki744 <zackisjostrom@gmail.com>
2024-10-23 13:17:09 +0200

    minor changes

--- blur_par/verify.sh
+++ blur_par/verify.sh
@@ -12,7 +12,7 @@ do
     do
         ./blur_par 15 "data/$image.ppm" "./data_o/blur_${image}_par.ppm" $thread
 
-        if ! cmp -s "./data_o/blur_${image}.ppm" "./data_o/blur_${image}_par.ppm"
+        if ! cmp -s "./data_o/blur_${image}_seq.ppm" "./data_o/blur_${image}_par.ppm"
         then
             echo "${red}Error: Incongruent output data detected when blurring image $image.ppm with $thread thread(s)${reset}"
             status=1
